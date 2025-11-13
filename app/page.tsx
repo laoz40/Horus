1ed6a3cbe17273f2828adad1275aad55f7069848
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2025-11-13 18:44:33 +1100

    Better tweakcn theme, dark mode toggle, added cursor pointer

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function DashboardPage() {
 	return (
 		<>
-			<h1 className="justify-center">Dashboard</h1>
-			<div className="flex flex-col pl-4 pr-4">
-				<h2 className="text-gray-500">Start</h2>
-				<div className="flex flex-row justify-center align-center w-full gap-1">
-					<Button className="flex-1">Create Workout</Button>
-					<Button variant="secondary" className="flex-1">Create a Preset</Button>
-				</div>
+			<div className="flex flex-row justify-between p-4">
+				<h1 className="justify-center">Dashboard</h1>
+				<ModeToggle></ModeToggle>
 			</div>
+			<section className="flex flex-col gap-1 pr-4 pl-4">
+				<h2 className="text-gray-500">Start</h2>
+				<div className="align-center flex w-full flex-row justify-center gap-2">
+					<Button
+						asChild
+						className="w-full flex-1">
+						<Link
+							href="/workout">
+							Create Workout
+						</Link>
+					</Button>
+
+					<Button
+						asChild
+						variant="secondary"
+						className="w-full flex-1">
+						<Link
+							href="#preset">
+							Create a Preset
+						</Link>
+					</Button>
+				</div>
+			</section>
 		</>
 	);
 }
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,10 @@
-import ThemeToggleButton from "@/components/button-16";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function SettingsPage() {
 	return (
 		<>
 			<h1>Settings</h1>
+			<ModeToggle></ModeToggle>
 		</>
 	);
 }
