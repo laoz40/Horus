893635b74d1fd11ce2b9e15fc8485619b4671ff8
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2025-11-19 18:56:30 +1100

    Small change to border styling

--- app/(no-navbar)/workout/page.tsx
+++ app/(no-navbar)/workout/page.tsx
@@ -10,7 +10,7 @@ export default function CreateWorkoutPage() {
 	return (
 		<>
 			{/* Top Actions */}
-			<div className="flex flex-row justify-between p-4 bg-input/50">
+			<div className="flex flex-row justify-between p-4 bg-input/50 dark:backdrop-blur-xs">
 				<Button
 					variant="secondary"
 					asChild
@@ -25,7 +25,7 @@ export default function CreateWorkoutPage() {
 			</div>
 
 			{/* Workout Name */}
-			<div className="flex flex-col gap-1 pl-4 pr-4 pb-4 border-b bg-input/50">
+			<div className="flex flex-col gap-1 pl-4 pr-4 pb-4 border-b bg-input/50 dark:backdrop-blur-xs">
 				<Input autoFocus placeholder="Workout Name" />
 				<div className="flex flex-row justify-between pl-3 pr-3">
 					<span className="text-muted-foreground text-sm">14. Nov 2025</span>
@@ -90,7 +90,7 @@ export default function CreateWorkoutPage() {
 			</div>
 
 			{/* Add Exercise */}
-			<div className="flex w-full border-t p-4 bg-input/50">
+			<div className="flex w-full border-t p-4 bg-input/50 dark:backdrop-blur-xs">
 				<Button 
 					className="flex-1">
 					+ Exercise
