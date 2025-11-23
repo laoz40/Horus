1377dfb57606cd41f2a123edefb5f6eff30086b3
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2025-11-23 20:49:37 +1100

    Slight edit to WorkoutCardStats

--- components/WorkoutCardStats.tsx
+++ components/WorkoutCardStats.tsx
@@ -3,31 +3,24 @@ import { type ReactElement } from "react";
 
 export default function WorkoutCardStats(): ReactElement {
 	return (
-<div className="grid grid-cols-3 mt-2 ">
-    {/* 1. Number of Exercises: Icon + Value (Horizontal) */}
-    <div className="flex items-center justify-center space-x-1.5 py-1">
-        <Dumbbell className="size-4 text-primary shrink-0" />
-        <span className="text-sm font-medium whitespace-nowrap">
-            5 Ex.
-        </span>
-    </div>
+		<div className="grid grid-cols-3 mt-4 pt-2 border-t">
+			{/* 1. Number of Exercises: Icon + Value (Horizontal) */}
+			<div className="flex items-center justify-center space-x-1.5 py-1">
+				<Dumbbell className="size-4 shrink-0" />
+				<span className="text-sm font-medium whitespace-nowrap">5</span>
+			</div>
 
-    {/* 2. Total Volume Lifted: Icon + Value (Horizontal) */}
-    <div className="flex items-center justify-center space-x-1.5 py-1">
-        <Weight className="size-4 text-primary shrink-0" />
-        <span className="text-sm font-medium whitespace-nowrap">
-            1215 kg
-        </span>
-    </div>
+			{/* 2. Total Volume Lifted: Icon + Value (Horizontal) */}
+			<div className="flex items-center justify-center space-x-1.5 py-1">
+				<Weight className="size-4 shrink-0" />
+				<span className="text-sm font-medium whitespace-nowrap">1215 kg</span>
+			</div>
 
-    {/* 3. Workout Duration: Icon + Value (Horizontal) */}
-    <div className="flex items-center justify-center space-x-1.5 py-1">
-        <Clock className="size-4 text-primary shrink-0" />
-        <span className="text-sm font-medium whitespace-nowrap">
-            45 min
-        </span>
-    </div>
-
-</div>
+			{/* 3. Workout Duration: Icon + Value (Horizontal) */}
+			<div className="flex items-center justify-center space-x-1.5 py-1">
+				<Clock className="size-4 shrink-0" />
+				<span className="text-sm font-medium whitespace-nowrap">45 min</span>
+			</div>
+		</div>
 	);
 }
