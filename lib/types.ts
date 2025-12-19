4158af6e3be866d5af4cca2b76e44a2b6b61b8e1
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2025-12-19 19:00:49 +1100

    Refactored WorkoutFormData type to repeat less code

--- lib/types.ts
+++ lib/types.ts
@@ -4,30 +4,20 @@ export type WorkoutWithRelations = PrismaWorkout & {
 	exercises: (Exercise & { sets: Set[] })[];
 };
 
-export type WorkoutFormData = {
-	name: string;
-	durationSeconds: number | null;
-	exercises: {
-		id: string;
-		name: string;
-		difficulty: number | null;
-		notes: string | null;
-		sets: {
-			id: string;
-			weight: string;
-			reps: string;
-		}[];
-	}[];
-};
-
 export interface ExerciseFormData {
-	name: string;
-	sets: {
-		weight: string;
-		reps: string;
-		id: string;
-	}[];
-	difficulty: number | null;
-	notes: string | null;
-	id: string;
+    id: string;
+    name: string;
+    difficulty: number | null;
+    notes: string | null;
+    sets: {
+        id: string;
+        weight: string;
+        reps: string;
+    }[];
+}
+
+export interface WorkoutFormData {
+    name: string;
+    durationSeconds: number | null;
+    exercises: ExerciseFormData[];
 }
