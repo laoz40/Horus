5ee048a0ebc9295062716b465621976b37d464f1
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-02-12 17:40:32 +1100

    style(layout): adjust dark background gradients

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,10 @@ export default function RootLayout({
 					className="absolute inset-0 z-0 hidden dark:block"
 					style={{
 						background: `
-          radial-gradient(ellipse 70% 55% at 50% 50%, rgba(50, 20, 147, 0.15), transparent 50%),
-            radial-gradient(ellipse 160% 130% at 10% 10%, rgba(0, 255, 255, 0.12), transparent 60%),
-            radial-gradient(ellipse 160% 130% at 90% 90%, rgba(38, 43, 226, 0.18), transparent 65%),
-            radial-gradient(ellipse 110% 50% at 80% 30%, rgba(255, 215, 0, 0.08), transparent 40%),
+          radial-gradient(ellipse 70% 55% at 20% 50%, rgba(50, 20, 147, 0.15), transparent 60%),
+            radial-gradient(ellipse 160% 130% at 30% 30%, rgba(0, 255, 255, 0.12), transparent 40%),
+            radial-gradient(ellipse 160% 130% at 70% 90%, rgba(38, 43, 226, 0.18), transparent 35%),
+            radial-gradient(ellipse 110% 50% at 80% 30%, rgba(255, 215, 0, 0.08), transparent 30%),
             #000000
      `,
 					}}
