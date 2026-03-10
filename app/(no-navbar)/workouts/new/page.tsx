6e1fa91ee1695c460102fabb1fdd947358a49e83
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-03-10 17:31:56 +1100

    perf: preload user check on server for sign up dialog

--- app/(no-navbar)/workouts/new/page.tsx
+++ app/(no-navbar)/workouts/new/page.tsx
@@ -1,10 +1,14 @@
 import SignUpDialog from "@/features/auth/components/SignUpDialog";
 import WorkoutForm from "@/features/workout-form/components/WorkoutForm";
+import { api } from "@/convex/_generated/api";
+import { fetchAuthQuery } from "@/lib/auth-server";
 
-export default function CreateWorkoutPage() {
+export default async function CreateWorkoutPage() {
+	const user = await fetchAuthQuery(api.auth.getCurrentUser);
+
 	return (
 		<>
-			<SignUpDialog />
+			<SignUpDialog show={!user} />
 			<WorkoutForm />
 		</>
 	);
--- features/auth/components/SignUpDialog.tsx
+++ features/auth/components/SignUpDialog.tsx
@@ -11,36 +11,38 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { showErrorToast } from "@/lib/toastMessages";
-import { Unauthenticated } from "convex/react";
 import Link from "next/link";
 import { type ReactElement } from "react";
 
-export default function SignUpDialog(): ReactElement {
+interface SignUpDialogProps {
+	show: boolean;
+}
+
+export default function SignUpDialog({ show }: SignUpDialogProps): ReactElement {
+	if (!show) {
+		return <></>;
+	}
 
 	const handleClick = () => {
 		showErrorToast("Any progress will not be saved.");
 	};
 
 	return (
-		<>
-			<Unauthenticated>
-				<AlertDialog defaultOpen>
-					<AlertDialogContent size="sm">
-						<AlertDialogHeader>
-							<AlertDialogTitle>Not Signed In</AlertDialogTitle>
-							<AlertDialogDescription className="text-balance">
-								Your workouts cannot be saved without an account.
-							</AlertDialogDescription>
-						</AlertDialogHeader>
-						<AlertDialogFooter>
-							<AlertDialogCancel variant="secondary" onClick={handleClick}>Let me test</AlertDialogCancel>
-							<Button asChild>
-								<Link href="/login">Sign in</Link>
-							</Button>
-						</AlertDialogFooter>
-					</AlertDialogContent>
-				</AlertDialog>
-			</Unauthenticated>
-		</>
+		<AlertDialog defaultOpen>
+			<AlertDialogContent size="sm">
+				<AlertDialogHeader>
+					<AlertDialogTitle>Not Signed In</AlertDialogTitle>
+					<AlertDialogDescription className="text-balance">
+						Your workouts cannot be saved without an account.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel variant="secondary" onClick={handleClick}>Let me test</AlertDialogCancel>
+					<Button asChild>
+						<Link href="/login">Sign in</Link>
+					</Button>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	);
 }
