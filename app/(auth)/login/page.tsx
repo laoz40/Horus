fb300bd1c722c2c77868ce8a7078590024e958fb
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-03-11 16:24:09 +1100

    style: fix OTP input UI
    - Such a ridiculous workaround and waste of time

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,12 +6,17 @@ export default function SignInPage() {
 			<AuthView
 				view="EMAIL_OTP"
 				redirectTo="/welcome"
-				cardFooter="Thanks for using my app!"
 				cardHeader="Log in to Horus"
+				cardFooter="Thanks for using my app, and have fun with your workout!"
 				classNames={{
 					base: "rounded-none bg-transparent border-none shadow-none",
 					header: "text-center text-xl font-semibold",
-					footer: "text-center text-xs text-muted-foreground"
+					footer: "text-xs text-muted-foreground",
+					form: {
+						// target descendant elements (so ridiculous! why not give the actual component when it uses shadcn??)
+						otpInputContainer:
+							"[&_[data-slot=input-otp-group]]:w-full [&_[data-slot=input-otp-slot]]:flex-1 [&_[data-slot=input-otp-slot]]:h-9",
+					},
 				}}
 			/>
 		</div>
