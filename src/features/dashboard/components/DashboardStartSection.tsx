3ac6cd14efa2605bae3adcca5ff195146edaa98f
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-09-12 19:37:56 +1000

    style: add icons to dashboard start buttons and workout card menu

--- src/features/dashboard/components/DashboardStartSection.tsx
+++ src/features/dashboard/components/DashboardStartSection.tsx
@@ -3,6 +3,7 @@
 import Section from "@/components/Section";
 import { Button } from "@/components/ui/button";
 import { showInfoToast } from "@/lib/toastMessages";
+import { IconBoltFilled, IconListNumbers } from "@tabler/icons-react";
 import Link from "next/link";
 
 export default function DashboardStartSection() {
@@ -12,8 +13,11 @@ export default function DashboardStartSection() {
 				<Button
 					asChild
 					size="lg"
-					className="flex-1">
-					<Link href="/workouts/new">Start Workout</Link>
+					className="flex-1 gap-1!">
+					<Link href="/workouts/new">
+						<IconBoltFilled className="size-4" />
+						Start Workout
+					</Link>
 				</Button>
 
 				<Button
@@ -21,8 +25,11 @@ export default function DashboardStartSection() {
 					onClick={() => showInfoToast("Presets are coming soon.")}
 					variant="outline"
 					size="lg"
-					className="flex-1">
-					<Link href="#preset">Create a Preset</Link>
+					className="flex-1 gap-1!">
+					<Link href="#preset">
+						<IconListNumbers className="size-4" />
+						Create a Preset
+					</Link>
 				</Button>
 			</div>
 		</Section>
